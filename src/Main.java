0c9a34b89a26682439959e777494308b3588ed72
NicoDev <gnicolasalvarezf@gmail.com>
2023-12-18 23:03:38 -0300

    Pegunta edad

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         Persona persona = new Persona("Nicola");
         System.out.println("Hello Git! Soy: " + persona.getName());
+        System.out.println("Cuantos años tienes?");
+        var age = entrada.nextInt();
+        System.out.println("Ok..tienes "+ age + " años");
     }
 }
